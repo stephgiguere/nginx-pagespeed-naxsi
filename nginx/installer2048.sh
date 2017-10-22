bb4b6d9c0972199d53738d15423b21cfc672909c
Stephane <stephgiguere@gmail.com>
2017-10-22 19:50:02 -0400

    Update installer2048.sh

--- nginx/installer2048.sh
+++ nginx/installer2048.sh
@@ -2,10 +2,10 @@
 #+----------------------------------------------------------------------------+
 #+ ServerAdmin NGINX (Mainline) Auto-Installer for Ubuntu
 #+----------------------------------------------------------------------------+
-#+ Author:      Jonathan Tittle
+#+ Author:      Jonathan Tittle (updated by twog.tech)
 #+ Copyright:   2017 ServerAdmin.sh and Jonathan Tittle
-#+ GitHub:      https://github.com/serveradminsh/serveradmin-installers
-#+ Issues:      https://github.com/serveradminsh/serveradmin-installers/issues
+#+ GitHub:      https://github.com/twogtech/nginx-pagespeed-naxsi
+#+ Issues:      https://github.com/twogtech/nginx-pagespeed-naxsi/issues
 #+ License:     GPL v3.0
 #+ OS:          Ubuntu 16.04, Ubuntu 16.10
 #+ Release:     1.0.0
